a17d144eaf69a0fd48cdc31c657ec94c60ccfa1b
diego <diego@diego-HP-240-G5-Notebook-PC>
2019-04-03 16:45:44 -0600

    Structs

---
+++ ProyectoSO/src/structs/Este.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Este
+{
+    int k_carros_e = 300;
+    int pro_lle = 5;
+    int tie_sem = 6;
+    int vel_min = 4;
+    int vel_min = 8;
+    int k_veh_x_pas = 7;
+    int k_amb = 4 * k_carros_e / 100;
+}
---
+++ ProyectoSO/src/structs/Puente.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Puente
+{
+    bool OE = 0, EO = 1;
+    int LonPue = 10;
+};
