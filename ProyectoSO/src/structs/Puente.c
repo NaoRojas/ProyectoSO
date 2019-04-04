2d524c5e462b2d27c8b5d26083c0fac7fc65fd88
nao <nao@nao-UX305LA>
2019-04-03 19:34:44 -0600

    sss

--- ProyectoSO/src/structs/Puente.c
+++ ProyectoSO/src/structs/Puente.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Oeste.c"
+#include "Este.c"
 
 struct Puente
 {
-    bool OE = 0, EO = 1;
+	Oeste ladoOeste;
+	Este ladoEste;
+    bool  direccion; //OE = 0, EO = 1;
     int LonPue = 10;
+    Puente(bool d){ direccion =d;}
+
 };
+
