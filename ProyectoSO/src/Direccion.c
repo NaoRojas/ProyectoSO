7ee16be17e89407a21a1c2ca18b5f4a4c26f446d
nao <nao@nao-UX305LA>
2019-04-03 20:39:03 -0600

    sszsszs

--- ProyectoSO/src/Direccion.c
+++ ProyectoSO/src/Direccion.c
@@ -4,13 +4,13 @@
 struct Direccion
 {
 	bool direccion; //OE 0 y EO 1
-    int k_carros_e = 288;
-    int pro_lle = 7;
-    int tie_sem = 3;
-    int vel_min = 12;
-    int vel_max = 30;
-    int k_veh_x_pas = 3;
-    int k_amb = 40 * 288 / 100;
+    int k_carros_e ;
+    int pro_lle ;
+    int tie_sem ;
+    int vel_min ;
+    int vel_max;
+    int k_veh_x_pas ;
+    int k_amb ;
 
 };
 struct Direccion *Oeste_new( bool direccion, int k_carros_e, int pro_lle, int tie_sem,
