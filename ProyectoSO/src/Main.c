4ce33bf914b86f71f8181dc7a29469838e7d490d
nao <nao@nao-UX305LA>
2019-04-07 20:33:06 -0600

    Ya sirve la recuperacion

--- ProyectoSO/src/Main.c
+++ ProyectoSO/src/Main.c
@@ -34,22 +34,52 @@ void* La_Ladrona_de_Libros(){
         exit(1);
     }
     fclose(fichero);
-    p.longitud = number[0];
-    e.k_carros_e = number[1];
-    e.pro_lle = number[2];
-    e.tie_sem = number[3];
-    e.vel_min = number[4];
-    e.vel_max = number[5];
-    e.k_veh_x_pas = number[6];
-    e.k_amb = number[7];
-    e.k_carros_e = number[1];
-    o.pro_lle = number[2];
-    o.tie_sem = number[3];
-    o.vel_min = number[4];
-    o.vel_max = number[5];
-    o.k_veh_x_pas = number[6];
-    o.k_amb = number[7];
+    struct Puente p;
+    struct PuntosCardinales o;
+    struct PuntosCardinales e;
+    p.longitud = number[0];		//Longitud de puente
+    //Datos este
+    e.k_carros_e = number[1];	//Cantidad de carros
+    e.pro_lle = number[2];		//Promedio de llegada
+    e.tie_sem = number[3];		//Tiempo de Semaforo
+    e.vel_min = number[4];		//Velocidad Minima
+    e.vel_max = number[5];		//Velocidad Maxima
+    e.k_veh_x_pas = number[6];	//Cantidad de vehiculos por paso
+    e.k_amb = number[7];		//Prcentaje de ambulancias
+    //Datos Oeste
+
+    o.k_carros_e = number[1];	//Cantidad de carros
+      o.pro_lle = number[2];		//Promedio de llegada
+      o.tie_sem = number[3];		//Tiempo de Semaforo
+      o.vel_min = number[4];		//Velocidad Minima
+      o.vel_max = number[5];		//Velocidad Maxima
+      o.k_veh_x_pas = number[6];	//Cantidad de vehiculos por paso
+      o.k_amb = number[7];		//Prcentaje de ambulancias
+
+      printf("Longitud de Puente %d \n"
+    		  "Datos Este  \n"
+    		  "Cantidad de carros %d \n"
+    		  "Promedio de llegada %d \n"
+    		  "Tiempo de semaforo %d  \n"
+    		  "Velocidad Maxima  %d \n"
+    		  "Vecidad Minima %d  \n"
+    		  "Cantidad de vehiculos por paso %d  \n"
+    		  "Porcentaje de ambulancias %d \n",
+			  p.longitud, e.k_carros_e,e.pro_lle,e.tie_sem,  e.vel_min,  e.vel_max,  e.k_veh_x_pas,e.k_amb);
+
+      printf(  "Datos Oeste  \n"
+        		  "Cantidad de carros %d \n"
+        		  "Promedio de llegada %d \n"
+        		  "Tiempo de semaforo %d  \n"
+        		  "Velocidad Maxima  %d \n"
+        		  "Vecidad Minima %d  \n"
+        		  "Cantidad de vehiculos por paso %d  \n"
+        		  "Porcentaje de ambulancias %d \n",
+    			 o.k_carros_e,o.pro_lle,o.tie_sem,  o.vel_min,  o.vel_max,  o.k_veh_x_pas,o.k_amb);
+
+
 }
+
 
 int main() {
     La_Ladrona_de_Libros();
